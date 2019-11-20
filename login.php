8033403cce4ff2359129f84336b3e2dc75b1fe31
yigu-new <57757589+yigu-new@users.noreply.github.com>
2019-11-20 11:43:25 +0800

    Add files via upload

--- login.php
+++ login.php
@@ -17,12 +17,12 @@
 	echo '数据库连接失败！';        
 	exit(0);    }
 	else{        
-	$id=$_POST['txtUsername'];
-	$password=$_POST['txtPassword'];
-	$result=mysqli_query($conn,"select*from id_password where id='$id' and password='$password'and identity='user'");       
+	$id=$_POST['username'];
+	$password=$_POST['password'];
+	$result=mysqli_query($conn,"select*from id_password where id='$id' and password='$password'and identity='staff'");       
 	$number = mysqli_num_rows($result);        
 	if ($number) {            
-	echo '<script>window.location="index.html";</script>';        } 
+	echo '<script>window.location="admin_index.html";</script>';        } 
 	else {            
 	echo '<script>alert("ID或密码错误！");history.go(-1);</script>';        
 	}   
