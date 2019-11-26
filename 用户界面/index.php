8c3e859fa2ad2fe19fb8536078066ed11b9d82d1
yigu-new <57757589+yigu-new@users.noreply.github.com>
2019-11-26 20:28:24 +0800

    更新版首页

--- 用户界面/index.php
+++ 用户界面/index.php
@@ -52,82 +52,45 @@
         <div class="goods_count fl" id="show_count">0</div>
     </div>
 </div>
-
-
-<div class="navbar_con">
-    <div class="navbar">
-        <h1 class="fl">全部商品分类</h1>
-
-    </div>
-</div>
-
 <div class="center_con clearfix">
-    <ul class="subnav fl">
-        <li><a href="#" >学习书刊</a></li>
-        <li><a href="#" >小说书刊</a></li>
-        <li><a href="#" >时尚杂志</a></li>
-        <li><a href="#" >儿童书刊</a></li>
-        <li><a href="#" >明星传记</a></li>
-        <li><a href="#" >励志书刊</a></li>
-    </ul>
-    <div class="container">
-        <div class="wrap"  style="left:-802px;">
-            <img src="images/5.webp">
-            <img src="images/1.webp">
-            <img src="images/2.webp">
-            <img src="images/3.webp">
-            <img src="images/4.webp">
-            <img src="images/5.webp">
-            <img src="images/1.webp">
-        </div>
-        <ul class="tab-nav" style="margin-left: -78px; display: block;">
-            <s class="nav-item" data-tab-idx="0"></s>
-            <s class="nav-item " data-tab-idx="1"></s>
-            <s class="nav-item" data-tab-idx="2"></s>
-            <s class="nav-item" data-tab-idx="3"></s>
-            <s class="nav-item" data-tab-idx="4"></s>
-
-        </ul>
-        <div class="tab-prev" id='tab-prev'contenteditable="true"></div>
-        <div class="tab-next" id='tab-next'contenteditable="true"></div>
-    </div>
-    <div class="right_body">
-        <img src="images/6.jpg">
-        <img id="top_img" src="images/7.jpg">
-
-    </div>
-
-    <div class="list_model">
+<img src="images/index_ad.jpg" width="739" height="423"  style="margin-left:20%; border: 5px solid orangered">
+<div class="list_model">
         <div class="list_title clearfix">
-            <h3 class="fl" id="model01">学习书刊</h3>
+            <h3 class="fl" id="model01">文学</h3>
             <div class="subtitle fl">
                 <span>|</span>
             </div>
             <a href="#" class="goods_more fr" >查看更多 ></a>
-        </div>
+  </div>
 
         <div class="goods_con clearfix">
 
             <ul  class="goods_list fl">
                 <span></span>
-                {% for book1 in book1s%}
+                <?php
+                include "db.php";
+                $sql1="SELECT*from book_info where book_type='文学'";
+                $r1=mysqli_query($conn,$sql1);
+                for($x=0; $x<=3; $x++) {
+                    $row1=mysqli_fetch_array($r1);
+                ?>
                 <li>
 
-                    <h4><a href="{% url 'bShop:detail' %}?pid={{ book1.pid }}">{{ book1.pdname}}</a></h4>
-                    {% with imgl=book1.pdImage %}
-                    <a href="{% url 'bShop:detail' %}?pid={{ book1.pid }}"><img src="{{ imgl }}"></a>
-                    {% endwith %}
-                    <div class="prize">¥ {{ book1.pdprice }}</div>
+                    <h4><a href="detail.php?id=id=<?php echo $row1["book_id"]?>"><?php echo $row1["book_name"] ?></a></h4>
+                    <a href="detail.php?id=id=<?php echo $row1["book_id"]?>"><img src=<?php echo $row1["book_picture"]?>></a>
+                    <div class="prize"><?php echo $row1["book_sale_price"]."元"?></div>
                 </li>
-                {% endfor %}
+                    <?php
+                }
+                ?>
             </ul>
         </div>
 
-    </div>
+</div>
 
     <div class="list_model">
         <div class="list_title clearfix">
-            <h3 class="fl" id="model01">小说书刊</h3>
+            <h3 class="fl" id="model01">经济</h3>
             <div class="subtitle fl">
                 <span>|</span>
             </div>
@@ -139,22 +102,28 @@
 
             <ul  class="goods_list fl">
                 <span></span>
-                {% for book2 in book2s%}
-                <li>
-                    <h4><a href="{% url 'bShop:detail' %}?pid={{ book2.pid }}">{{ book2.pdname}}</a></h4>
-                    {% with imgl2=book2.pdImage %}
-                    <a href="{% url 'bShop:detail' %}?pid={{ book2.pid }}"><img src="{{ imgl2 }}"></a>
-                    {% endwith %}
-                    <div class="prize">¥ {{ book2.pdprice }}</div>
-                </li>
-                {% endfor %}
+                <?php
+                $sql2="SELECT*from book_info where book_type='经济' or book_type='投资'";
+                $r2=mysqli_query($conn,$sql2);
+                for($x=0; $x<=3; $x++) {
+                    $row2=mysqli_fetch_array($r2);
+                    ?>
+                    <li>
+
+                        <h4><a href="detail.php?id=id=<?php echo $row2["book_id"]?>"><?php echo $row2["book_name"] ?></a></h4>
+                        <a href="detail.php?id=id=<?php echo $row2["book_id"]?>"><img src=<?php echo $row2["book_picture"]?>></a>
+                        <div class="prize"><?php echo $row2["book_sale_price"]."元"?></div>
+                    </li>
+                    <?php
+                }
+                ?>
             </ul>
         </div>
 
     </div>
     <div class="list_model">
         <div class="list_title clearfix">
-            <h3 class="fl" id="model01">时尚杂志</h3>
+            <h3 class="fl" id="model01">教辅</h3>
             <div class="subtitle fl">
                 <span>|</span>
             </div>
@@ -166,22 +135,28 @@
 
             <ul  class="goods_list fl">
                 <span></span>
-                {% for book3 in book3s%}
-                <li>
-                    <h4><a href="{% url 'bShop:detail' %}?pid={{ book3.pid }}">{{ book3.pdname}}</a></h4>
-                    {% with imgl3=book3.pdImage %}
-                    <a href="{% url 'bShop:detail' %}?pid={{ book3.pid }}"><img src="{{ imgl3 }}"></a>
-                    {% endwith %}
-                    <div class="prize">¥ {{ book3.pdprice }}</div>
-                </li>
-                {% endfor %}
+                <?php
+                $sql3="SELECT*from book_info where book_type='数学' or book_type='计算机' or  book_type='艺术'";
+                $r3=mysqli_query($conn,$sql3);
+                for($x=0; $x<=3; $x++) {
+                    $row3=mysqli_fetch_array($r3);
+                    ?>
+                    <li>
+
+                        <h4><a href="detail.php?id=id=<?php echo $row3["book_id"]?>"><?php echo $row3["book_name"] ?></a></h4>
+                        <a href="detail.php?id=id=<?php echo $row3["book_id"]?>"><img src=<?php echo $row3["book_picture"]?>></a>
+                        <div class="prize"><?php echo $row3["book_sale_price"]."元"?></div>
+                    </li>
+                    <?php
+                }
+                ?>
             </ul>
         </div>
 
     </div>
     <div class="list_model">
         <div class="list_title clearfix">
-            <h3 class="fl" id="model01">儿童书刊</h3>
+            <h3 class="fl" id="model01">史哲</h3>
             <div class="subtitle fl">
                 <span>|</span>
             </div>
@@ -193,73 +168,39 @@
 
             <ul  class="goods_list fl">
                 <span></span>
-                {% for book4 in book4s%}
-                <li>
-                    <h4><a href="{% url 'bShop:detail' %}?pid={{ book4.pid }}">{{ book4.pdname}}</a></h4>
-                    {% with imgl4=book4.pdImage %}
-                    <a href="{% url 'bShop:detail' %}?pid={{ book4.pid }}"><img src="{{ imgl4 }}"></a>
-                    {% endwith %}
-                    <div class="prize">¥ {{ book4.pdprice }}</div>
-                </li>
-                {% endfor %}
+                <?php
+                $sql4="SELECT*from book_info where book_type='哲学' or book_type='历史' or  book_type='心理学'";
+                $r4=mysqli_query($conn,$sql4);
+                for($x=0; $x<=3; $x++) {
+                    $row4=mysqli_fetch_array($r4);
+                    ?>
+                    <li>
+
+                        <h4><a href="detail.php?id=id=<?php echo $row4["book_id"]?>"><?php echo $row4["book_name"] ?></a></h4>
+                        <a href="detail.php?id=id=<?php echo $row4["book_id"]?>"><img src=<?php echo $row4["book_picture"]?>></a>
+                        <div class="prize"><?php echo $row4["book_sale_price"]."元"?></div>
+                    </li>
+                    <?php
+                }
+                ?>
             </ul>
         </div>
 
     </div>
-    <div class="list_model">
-        <div class="list_title clearfix">
-            <h3 class="fl" id="model01">明星传记</h3>
-            <div class="subtitle fl">
-                <span>|</span>
-            </div>
-            <a href="#" class="goods_more fr" id="fruit_more">查看更多 ></a>
-        </div>
 
 
         <div class="goods_con clearfix">
 
             <ul  class="goods_list fl">
                 <span></span>
-                {% for book5 in book5s%}
-                <li>
-                    <h4><a href="{% url 'bShop:detail' %}?pid={{ book5.pid }}">{{ book5.pdname}}</a></h4>
-                    {% with imgl5=book5.pdImage %}
-                    <a href="{% url 'bShop:detail' %}?pid={{ book5.pid }}"><img src="{{ imgl5 }}"></a>
-                    {% endwith %}
-                    <div class="prize">¥ {{ book5.pdprice }}</div>
-                </li>
-                {% endfor %}
+
+
             </ul>
         </div>
 
     </div>
-    <div class="list_model">
-        <div class="list_title clearfix">
-            <h3 class="fl" id="model01">励志书刊</h3>
-            <div class="subtitle fl">
-                <span>|</span>
-            </div>
-            <a href="#" class="goods_more fr" id="fruit_more">查看更多 ></a>
-        </div>
 
 
-        <div class="goods_con clearfix">
-
-            <ul  class="goods_list fl">
-                <span></span>
-                {% for book6 in book6s%}
-                <li>
-                    <h4><a href="{% url 'bShop:detail' %}?pid={{ book6.pid }}">{{ book6.pdname}}</a></h4>
-                    {% with imgl6=book6.pdImage %}
-                    <a href="{% url 'bShop:detail' %}?pid={{ book6.pid }}"><img src="{{ imgl6 }}"></a>
-                    {% endwith %}
-                    <div class="prize">¥ {{ book6.pdprice }}</div>
-                </li>
-                {% endfor %}
-            </ul>
-        </div>
-
-    </div>
 
 
     <script type="text/javascript" src="js/editpwd.js"></script>
