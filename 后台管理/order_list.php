8737ef945d711e1fe5b357056c40832948775ea7
董七 <57610263+ZoePKU@users.noreply.github.com>
2019-11-28 15:30:56 +0800

    增加了订单列表

---
+++ 后台管理/order_list.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <title>奇文书坊后台管理系统</title>
+    <meta name="author" content="DeathGhost" />
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+    <!--[if lt IE 9]>
+    <script src="{% static 'admin/js/html5.js' %}"></script>
+    <![endif]-->
+    <script src="js/jquery.js"></script>
+    <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+    <script>
+        (function($){
+            $(window).load(function(){
+                $("a[rel='load-content']").click(function(e){
+                    e.preventDefault();
+                    var url=$(this).attr("href");
+                    $.get(url,function(data){
+                        $(".content .mCSB_container").append(data); //load new content inside .mCSB_container
+                        //scroll-to appended content
+                        $(".content").mCustomScrollbar("scrollTo","h2:last");
+                    });
+                });
+                $(".content").delegate("a[href='top']","click",function(e){
+                    e.preventDefault();
+                    $(".content").mCustomScrollbar("scrollTo",$(this).attr("href"));
+                });
+            });
+        })(jQuery);
+    </script>
+</head>
+<body>
+<!--header-->
+<header>
+    <h1><img src="images/admin_logo.png"/></h1>
+    <ul class="rt_nav">
+        <li><a href="admin_index.php" class="website_icon">站点首页</a></li>
+        <li><a href="admin_logout.php" class="quit_icon">安全退出</a></li>
+    </ul>
+</header>
+<!--aside nav-->
+<!--aside nav-->
+<aside class="lt_aside_nav content mCustomScrollbar">
+    <h2>
+        <?php
+        session_start();
+        echo'超级管理员：'.$_SESSION['staff_name'];
+        ?>
+    </h2>
+    <ul>
+        <li>
+            <dl>
+                <!--当前链接则添加class:active-->
+                <dt>商品管理</dt>
+                <dd><a href="addproduct.php">书目添加</a></dd>
+                <dd><a href="product_list.php">书目列表/修改/补货</a></dd>
+            </dl>
+        </li>
+        <li>
+            <dl>
+                <dt>订单管理</dt>
+                <dd><a href="order_list.php" class="active1">订单列表</a></dd>
+                <dd><a href="#">订单详情</a></dd>
+            </dl>
+        </li>
+
+        <li>
+            <p class="btm_infor">© 奇文书坊出品</p>
+        </li>
+    </ul>
+</aside>
+
+<section class="rt_wrap content mCustomScrollbar">
+    <div class="rt_content">
+        <div class="page_title">
+            <h2 class="fl">订单列表</h2>
+        </div>
+        <section class="mtb">
+
+            <input type="text" class="textbox textbox_225" placeholder="输入订单号..."/>
+            <input type="button" value="查询" class="group_btn"/>
+        </section>
+        <table class="table">
+            <tr>
+                <th>订单号</th>
+                <th>用户ID</th>
+                <th>负责员工ID</th>
+                <th>下单时间</th>
+            </tr>
+
+            {% for product in products %}
+            <tr>
+
+                <td class="center">{{ order_id }}</td>
+                <td class="center">{{ user_id }}</td>
+                <td class="center">{{ staff_id }}</td>
+                <td class="center">{{ order_time }}</td>
+
+            </tr>
+            {% empty %}
+            {% endfor %}
+        </table>
+        <aside class="paging">
+            {% if page.has_previous  %}
+            <a href="{% url 'Admin:product_list' %}?page_num={{ page.previous_page_number }}">上一页</a>
+            {% else %}
+            <a href="#">上一页</a>
+            {% endif %}
+
+            {% if page.has_next  %}
+            <a href="{% url 'Admin:product_list' %}?page_num={{ page.next_page_number }}">下一页</a>
+            {% else %}
+            <a href="#">下一页</a>
+            {% endif %}
+        </aside>
+    </div>
+</section>
+<script src="js/product_list.js" type="text/javascript"></script>
+</body>
+</html>
