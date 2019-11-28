56ff474f6288d681567e7fe55fa9d5579c1980b8
yigu-new <57757589+yigu-new@users.noreply.github.com>
2019-11-28 15:23:45 +0800

    Add files via upload

--- 后台管理/admin_login.php
+++ 后台管理/admin_login.php
@@ -18,6 +18,7 @@ if (!$row) {
 
 if ($row['staff_password'] == $password) {
     $_SESSION['staff_name'] = $row['staff_name'];
+	$_SESSION['staff_id'] = $row['staff_id'];
     echo "<script>
         alert('登录成功！正在跳转...')
         window.location.href=\"admin_index.php\";
