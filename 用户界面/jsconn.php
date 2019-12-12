e88067e07501b09a0ee3c348883860a60585aa14
Hogilala <50644334+Hogilala@users.noreply.github.com>
2019-12-12 21:16:39 +0800

    将mysql数据转成json格式，配套首页js数据可视化

---
+++ 用户界面/jsconn.php
@@ -0,0 +1,30 @@
+<?php
+
+require("show_test.php");
+$conn = mysqli_connect($mysql_server_name, $mysql_username, $mysql_password) or die("error connecting");
+mysqli_query($conn, "set names 'utf8'");
+mysqli_select_db($conn, $mysql_database);
+$result = mysqli_query($conn, "SELECT order_id,book_num,book_name FROM order_details,book_info WHERE order_details.book_id=book_info.book_id ");
+$data = "";
+$array = array();
+
+class User
+{
+    public $order_id;
+    public $book_name;
+    public $book_num;
+}
+
+while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+    $user = new User();
+    $user->order_id = $row['order_id'];
+    $user->book_name = $row['book_name'];
+    $user->book_num = $row['book_num'];
+    $array[] = $user;
+}
+$data = json_encode($array);
+// echo "{".'"user"'.":".$data."}";
+echo $data;
+
+?>
+
