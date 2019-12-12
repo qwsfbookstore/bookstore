d1d4f8b6d28bb516f3b33fd4af51cd7ee12577a5
yigu-new <57757589+yigu-new@users.noreply.github.com>
2019-12-12 23:49:41 +0800

    Update jsconn.php

--- 用户界面/jsconn.php
+++ 用户界面/jsconn.php
@@ -1,6 +1,6 @@
 <?php
 
-require("show_test.php");
+require("db.php");
 $conn = mysqli_connect($mysql_server_name, $mysql_username, $mysql_password) or die("error connecting");
 mysqli_query($conn, "set names 'utf8'");
 mysqli_select_db($conn, $mysql_database);
